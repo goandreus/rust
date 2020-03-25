849883fd94921a8c50e2e9dec476ed186e3be38e
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2020-03-24 23:58:33 -0500

    closures - input function

---
+++ functions7.rs
@@ -0,0 +1,16 @@
+fn call_me<F: Fn()>(f: F) {
+    f();
+}
+
+// Define a wrapper function satisfying the `Fn` bound
+fn function() {
+    println!("I'm a function!");
+}
+
+fn main() {
+    // Define a closure satisfying the `Fn` bound
+    let closure = || println!("I'm a closure!");
+
+    call_me(closure);
+    call_me(function);
+}
