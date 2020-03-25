a38b334a665f6a21ec7ee255099d570e064b1cce
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2020-03-24 23:57:53 -0500

    type anonymity

---
+++ functions6.rs
@@ -0,0 +1,22 @@
+fn apply<F>(f: F) where
+    F: FnOnce() {
+    f();
+}
+
+
+---------------
+
+fn apply<F>(f: F) where
+    F: Fn() {
+    f();
+}
+
+fn main() {
+    let x = 7;
+
+    // Capture `x` into an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
