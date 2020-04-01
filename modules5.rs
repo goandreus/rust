a5427e9c34f3f70b1f03c423defdee4878b6c6b9
Andres Chavez <andres.chavez.crisostomo@gmail.com>
2020-04-01 10:47:21 -0500

    Create modules5.rs
    $ tree .
    .
    |-- my
    |   |-- inaccessible.rs
    |   |-- mod.rs
    |   `-- nested.rs
    `-- split.rs
    
    
    $ rustc split.rs && ./split
    called `my::function()`
    called `function()`
    called `my::indirect_access()`, that
    > called `my::private_function()`
    called `my::nested::function()`

---
+++ modules5.rs
@@ -0,0 +1,50 @@
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
+---------------------------------
+// modules
+mod inaccessible;
+pub mod nested;
+
+pub fn function() {
+    println!("called `my::function()`");
+}
+
+fn private_function() {
+    println!("called `my::private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called `my::indirect_access()`, that\n> ");
+
+    private_function();
+}
+
+------------------------------------
+pub fn function() {
+    println!("called `my::nested::function()`");
+}
+
+#[allow(dead_code)]
+fn private_function() {
+    println!("called `my::nested::private_function()`");
+}
+
+---------------------------------------
+#[allow(dead_code)]
+pub fn public_function() {
+    println!("called `my::inaccessible::public_function()`");
+}
+-------------------------------------
